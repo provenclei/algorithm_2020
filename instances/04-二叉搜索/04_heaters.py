5e2855b37c222137f4a36c0fefda331a955b1d8a
MaoLongTL <18565663062@163.com>
2020-07-19 23:13:34 +0800

    commit binary_search_02

---
+++ instances/04-二叉搜索/04_heaters.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+"""
+@Author         :  LEITENG
+@Version        :  
+------------------------------------
+@File           :  04——heaters.py
+@Description    :  
+@CreateTime     :  2020/7/19 23:13
+------------------------------------
+@ModifyTime     :  
+"""
+
+
+def main():
+    pass
+
+
+if __name__ == '__main__':
+    main()
