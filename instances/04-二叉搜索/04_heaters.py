b920938698ffca10c9181795f1307fec5ae845a1
MaoLongTL <18565663062@163.com>
2020-07-24 00:25:03 +0800

    commit binary_search_03

--- instances/04-二叉搜索/04_heaters.py
+++ instances/04-二叉搜索/04_heaters.py
@@ -9,10 +9,28 @@
 ------------------------------------
 @ModifyTime     :  
 """
+from bisect import bisect
+
+
+def find_radius(houses: list, heaters: list):
+    heaters.sort()
+    redius = 0
+    for house in houses:
+        heater_idx = bisect(heaters, house)
+        left = heaters[heater_idx - 1] if heater_idx - 1 >= 0 else float('-inf')
+        right = heaters[heater_idx] if heater_idx < len(heaters) else float('inf')
+        redius = max(redius, min(house - left, right - house))
+    return redius
 
 
 def main():
-    pass
+    houses = [1, 2, 3, 4]
+    heaters = [1, 4]
+    print(find_radius(houses, heaters))
+
+    houses = [1, 12, 23, 34]
+    heaters = [12, 24]
+    print(find_radius(houses, heaters))
 
 
 if __name__ == '__main__':
