44242c7e8b3b7c0c47ff2c04c959b21a1a0bd2c7
MaoLongTL <18565663062@163.com>
2020-08-06 23:50:16 +0800

    commit buddyStrings

--- instances/其他/01_验证回文串.py
+++ instances/其他/01_验证回文串.py
@@ -11,18 +11,31 @@
 """
 
 
-# class Solution:
-#     def isPalindrome(self, s: str) -> bool:
-#         # 时间：o(n), 空间：o(n）
-#         sgood = ''.join(ch.lower() for ch in s if ch.isalnum())
-#         return sgood == sgood[::-1]
+class Solution1:
+    def isPalindrome(self, s: str) -> bool:
+        # 时间：o(n), 空间：o(n）
+
+        # 方法一
+        # sgood = ''.join(ch.lower() for ch in s if ch.isalnum())
+        # return sgood == sgood[::-1]
+
+        # 方法二：双指针
+        sgood = ''.join(ch.lower() for ch in s if ch.isalnum())
+        left, right = 0, len(sgood)-1
+        while left < right:
+            if sgood[left] != sgood[right]:
+                return False
+            else:
+                left += 1
+                right -= 1
+        return True
 
 
 class Solution:
     def isPalindrome(self, s: str) -> bool:
-        # 时间：o(n), 空间：o(n）
-        n = len(s)
-        left, right = 0, n - 1
+        # 时间：o(n), 空间：o(1）
+        # 在原字符串上进行
+        left, right = 0, len(s) - 1
         while left < right:
             while left < right and not s[left].isalnum():
                 left += 1
