2ca4f1d117b8d22e83431886e4bb7240b02171d2
MaoLongTL <18565663062@163.com>
2020-08-08 23:51:10 +0800

    commit 3sum-2

--- instances/其他/04_3sum.py
+++ instances/其他/04_3sum.py
@@ -7,12 +7,49 @@
 @Description    :  
 @CreateTime     :  2020/8/8 23:25
 ------------------------------------
-@ModifyTime     :  
+@ModifyTime     :
+    给你一个包含 n 个整数的数组 nums，判断 nums 中是否存在三个元素 a，b，c ，使得 a + b + c = 0 ？
+    请你找出所有满足条件且不重复的三元组。
+    注意：答案中不可以包含重复的三元组。
 """
 
 
+class Solution:
+    def threeSum(self, nums: list):
+        n = len(nums)
+        nums.sort()
+        ans = list()
+
+        # 枚举 a
+        for first in range(n):
+            # 需要和上一次枚举的数不相同
+            if first > 0 and nums[first] == nums[first - 1]:
+                continue
+            # c 对应的指针初始指向数组的最右端
+            third = n - 1
+            target = -nums[first]
+            # 枚举 b
+            for second in range(first + 1, n):
+                # 需要和上一次枚举的数不相同
+                if second > first + 1 and nums[second] == nums[second - 1]:
+                    continue
+                # 需要保证 b 的指针在 c 的指针的左侧
+                while second < third and nums[second] + nums[third] > target:
+                    third -= 1
+                # 如果指针重合，随着 b 后续的增加
+                # 就不会有满足 a+b+c=0 并且 b<c 的 c 了，可以退出循环
+                if second == third:
+                    break
+                if nums[second] + nums[third] == target:
+                    ans.append([nums[first], nums[second], nums[third]])
+
+        return ans
+
+
 def main():
-    pass
+    solution = Solution()
+    nums = [-1, 0, 1, 2, -1, -4]
+    print(solution.threeSum(nums))
 
 
 if __name__ == '__main__':
